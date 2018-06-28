845e4f7ec1368ef5bc99662c3a2e57fe51649321
Hui Yee <huiyee.chong@techstudio.com.sg>
2018-06-28 16:03:24 +0800

    TEST

---
+++ PotatoLibrary/PotatoLibrary/PotatoLibrary.h
@@ -0,0 +1,19 @@
+//
+//  PotatoLibrary.h
+//  PotatoLibrary
+//
+//  Created by Shaylee Chong on 28/6/18.
+//  Copyright © 2018 ShayleeChong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PotatoLibrary.
+FOUNDATION_EXPORT double PotatoLibraryVersionNumber;
+
+//! Project version string for PotatoLibrary.
+FOUNDATION_EXPORT const unsigned char PotatoLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PotatoLibrary/PublicHeader.h>
+
+
---
+++ PotatoLibrary/PotatoLibrary/service.swift
@@ -0,0 +1,20 @@
+//
+//  service.swift
+//  PotatoLibrary
+//
+//  Created by Shaylee Chong on 28/6/18.
+//  Copyright © 2018 ShayleeChong. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+    
+        return "DID SOMETHING"
+    }
+    
+}
